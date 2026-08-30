2f266dc616f791b4342c79f87f48854498b615ac
Alan Sales <alansales2018@gmail.com>
2026-08-30 18:54:13 -0300

    add 'Classes' and the start structure of Conta

---
+++ src/classes/Conta.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Conta {
+    private int numeroConta;
+    private String titularConta;
+    private String cpf;
+    private double saldo;
+
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
